877d063f4b4571bc780b557c566d30c36d52d32f
Caleb <kalebsam@hotmail.com>
2022-04-19 14:08:56 -0500

    fixes

--- projects/rekognition-fabioarias/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of rekognition-fabioarias
- */
-
-export * from './lib/rekognition-fabioarias.service';
-export * from './lib/rekognition-fabioarias.component';
-export * from './lib/rekognition-fabioarias.module';
